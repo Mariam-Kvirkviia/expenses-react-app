dc750ca24b3482e48b2bdd2059b1cc99ab41eb30
Mashawabupr <marikanik1999@gmail.com>
2022-11-22 13:30:37 +0300

    merge authentication site

---
+++ src/component/Auth/AuthForm.js
@@ -0,0 +1,93 @@
+import { useState, useEffect, useRef } from "react";
+
+import classes from "./AuthForm.module.css";
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  let emailRef = useRef();
+  let passwordRef = useRef();
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+  let handleSubmit = (event) => {
+    event.preventDefault();
+    let enteredEmail = emailRef.current.value;
+    let enteredPassword = passwordRef.current.value;
+    if (isLogin) {
+      fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAqB8ixB2ZHj_Sy3llzdg7OaYfiUc61pqU",
+        {
+          method: "POST",
+
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+        }
+      )
+        .then((response) => {
+          if (response.ok) {
+          } else {
+            return response.json().then((data) => {
+              throw new Error(data?.error?.message || "something went wrong!");
+            });
+          }
+        })
+        .catch((err) => {
+          alert(err);
+        });
+    } else {
+      fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAqB8ixB2ZHj_Sy3llzdg7OaYfiUc61pqU",
+        {
+          method: "POST",
+
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+        }
+      )
+        .then((response) => {
+          if (response.ok) {
+          } else {
+            return response.json().then((data) => {
+              throw new Error(data?.error?.message || "something went wrong!");
+            });
+          }
+        })
+        .catch((err) => {
+          alert(err);
+        });
+    }
+  };
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={handleSubmit}>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input type="password" id="password" required ref={passwordRef} />
+        </div>
+        <div className={classes.actions}>
+          <button>{isLogin ? "Login" : "Create Account"}</button>
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? "Create new account" : "Login with existing account"}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
